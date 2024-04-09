6d46a0c246c3154fb06490c4aa760dcfe8141d7d
PZagato <166504399+PZagato@users.noreply.github.com>
2024-04-09 07:40:00 -0300

    Create Ex8_8.py

---
+++ Ex8_8.py
@@ -0,0 +1,12 @@
+def somar_numeros(a, b):
+    return a + b
+
+# Exemplos de uso
+resultado1 = somar_numeros(5, 3)
+resultado2 = somar_numeros(10, -2)
+resultado3 = somar_numeros(7.5, 2.5)
+
+# Exibindo os resultados
+print("Resultado 1:", resultado1)
+print("Resultado 2:", resultado2)
+print("Resultado 3:", resultado3)
