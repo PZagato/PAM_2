7e4670aa8275aaf24e558dbd310580823af0af8d
PZagato <166504399+PZagato@users.noreply.github.com>
2024-04-09 07:39:09 -0300

    Create Ex5_8.js

---
+++ Ex5_8.js
@@ -0,0 +1,20 @@
+// Função Nomeada
+function minhaFuncao() {
+    // corpo da função
+}
+
+// Função Nomeada com argumento e retorno
+function soma(a, b) {
+    return a + b;
+}
+
+// Função com Construtor
+var minhaFuncaoConstrutor = new Function("x", "y", "return x * y");
+
+// Função Literal (atribuída a uma variável)
+var minhaFuncaoLiteral = function() {
+    // corpo da função
+};
+
+// Função de Flecha (Arrow Function)
+var somaArrow = (a, b) => a + b;
