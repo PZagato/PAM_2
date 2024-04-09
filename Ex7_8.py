728b794dd6d46041b0fb0ab014e72f46ca2afc45
PZagato <166504399+PZagato@users.noreply.github.com>
2024-04-09 07:39:35 -0300

    Create Ex7_8.py

---
+++ Ex7_8.py
@@ -0,0 +1,17 @@
+def converter_caixa_alta(texto):
+    return texto.upper()
+
+# Exemplos de uso
+string1 = "Olá, Mundo!"
+string2 = "Python é uma linguagem de programação poderosa."
+string3 = "Este é um exemplo de STRING com letras mistas."
+
+# Convertendo as strings para caixa alta
+resultado1 = converter_caixa_alta(string1)
+resultado2 = converter_caixa_alta(string2)
+resultado3 = converter_caixa_alta(string3)
+
+# Exibindo os resultados
+print("Resultado 1:", resultado1)
+print("Resultado 2:", resultado2)
+print("Resultado 3:", resultado3)
